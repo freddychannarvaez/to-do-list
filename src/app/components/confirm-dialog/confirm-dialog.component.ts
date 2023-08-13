44fa03e3cb1e335daa43551cc0e7725f23eb83f8
freddychannarvaez <freddy.chan.narvaez@gmail.com>
2023-08-12 19:48:35 -0600

    Created confirm dialog component

---
+++ src/app/components/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Inject, Output } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-confirm-dialog',
+  templateUrl: './confirm-dialog.component.html',
+  styleUrls: ['./confirm-dialog.component.scss']
+})
+export class ConfirmDialogComponent {
+
+  constructor(private dialogRef: MatDialogRef<ConfirmDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+ ) { }
+
+  onConfirmDialog() {
+    this.dialogRef.close(true);
+  }
+
+}
