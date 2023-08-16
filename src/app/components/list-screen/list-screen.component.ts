887b8012516de658a30eec0584e4db8ed9755049
freddychannarvaez <freddy.chan.narvaez@gmail.com>
2023-08-15 19:26:55 -0600

    Fixing list methods

--- src/app/components/list-screen/list-screen.component.ts
+++ src/app/components/list-screen/list-screen.component.ts
@@ -1,14 +1,15 @@
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component, EventEmitter, Input, Output, SimpleChanges, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, FormGroupDirective, Validators } from '@angular/forms';
-import { MatDialog, MatDialogRef } from '@angular/material/dialog';
-import { Subscription } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { Subscription, firstValueFrom } from 'rxjs';
 import { List } from 'src/app/common/models/list.model';
 import { Note } from 'src/app/common/models/note.model';
 import { ListService } from 'src/app/common/services/list.service';
 import { NoteService } from 'src/app/common/services/note.service';
 import { RenameDialogComponent } from '../rename-dialog/rename-dialog.component';
 import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-list-screen',
@@ -21,170 +22,196 @@ export class ListScreenComponent {
   @Input() isDraggable!: boolean;
   @ViewChild('theForm') theForm!: FormGroupDirective;
   @Output() newItemEvent = new EventEmitter<number>();
+  @Output() changedDailyStatus = new EventEmitter<{index: number, value: boolean}>();
+  @Output() changedCompletedStatus = new EventEmitter<{index: number, value: boolean}>();
   
   constructor(private fb: FormBuilder,
     private noteService: NoteService,
     private listService: ListService,
     private renameDialog: MatDialog,
+    private snackbar: MatSnackBar,
     private confirmDialog: MatDialog) {}
     
   title!: string;
-  
-  // notes = ['1', '2', '3'];
   notes: Note[] = [];
-  
   subscriptions: Subscription[] = [];
   
   formGroup: FormGroup = this.fb.group({
     newNote: new FormControl('', [Validators.required, Validators.pattern(/^(\s+\S+\s*)*(?!\s).*$/)]),
-    // newNote: new FormControl('', [Validators.required, Validators.pattern('^(?!\s*$).+')]),
-    // newNote: new FormControl('', [Validators.required, Validators.minLength(1)]),
   });
   
   ngOnInit() {
-    // this.buildForm();
-    // console.log(this.list);
-    // this.title = this.list?.title;
     this.subscriptions.push(
       this.noteService.getTodayNotes().subscribe((notes: Note[]) => {
-        this.notes = notes;
-        console.log("🚀 ~ file: list-screen.component.ts:32 ~ ListScreenComponent ~ this.noteService.getTodayNotes ~ this.notes:", this.notes)
         this.title = this.list?.title ?? 'All Notes';
-        console.log("🚀 ~ file: list-screen.component.ts:45 ~ ListScreenComponent ~ this.noteService.getTodayNotes ~ this.list:", this.list)
       })
       );
     }
       
   ngOnChanges(changes: SimpleChanges): void {
-    console.log("🚀 ~ file: list-screen.component.ts:19 ~ ListScreenComponent ~ isDraggable:", this.isDraggable)
-    console.log("🚀 ~ file: list-screen.component.ts:51 ~ ListScreenComponent ~ ngOnChanges ~ changes:", changes)
     //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
     //Add '${implements OnChanges}' to the class.
     this.title = this.list?.title;
-    console.log(this.list);
   }
 
-  // buildForm() {
-  //   this.formGroup = this.fb.group({
-  //     newNote: new FormControl('new note', [Validators.required]),
-  //   });
-  // }
-
-  createNote() {
+  /**
+   * Creates a note.
+   * @returns void.
+   */
+  createNote(): void {
     const newNote = this.formGroup.controls['newNote'];
-    // console.log(newNote.valid);
 
     if (!newNote.valid) return;
 
-    console.log('note created!', newNote.value);
-    // this.notes.push(this.noteService.create(newNote.value, this.notes.length + 1, true));
     this.subscriptions.push(
-      this.noteService.create(newNote.value, this.notes.length + 1, true)
+      this.noteService.create(newNote.value, this.result.length + 1, this.list?.id === -1 ? true : false)
         .subscribe((note: Note) => {
-          console.log("🚀 ~ file: list-screen.component.ts:59 ~ ListScreenComponent ~ .subscribe ~ note:", note)
-          this.notes.push(note);
-          this.result = [...this.notes]
-          newNote.reset();
-          this.theForm.resetForm();
+            this.result.push(note);
+            newNote.reset();
+            this.theForm.resetForm();
+
+            // Add note to the list
+            if (this.list?.id! >= 0) {
+              // Refresh all notes & updated lists
+              this.subscriptions.push(
+                this.listService
+                  .update(this.list.id!, {...this.list, notes: [...this.list.notes, note.id!]})
+                  .subscribe((x) => {
+                      this.noteService.updateNotesSubject([...this.result]);
+                      this.openSnackBar('Note created', 'Close');
+                  })
+              );
+            }
         })
     );
 
   }
 
-  editNote(value: number) {
-    console.log('editing note', value);
+  /**
+   * Emits the index of the edited note.
+   * @param value Index of the note.
+   * @returns void.
+   */
+  editNote(value: number): void {
     this.newItemEvent.emit(value);
   }
 
-
+  /**
+   * Reorders the notes array by dragging its elements.
+   * @param event Event of the drag & drop.
+   * @returns void.
+   */
   onDropNote(event: CdkDragDrop<Note[]>) {
-    console.log("🚀 ~ file: list-screen.component.ts:77 ~ ListScreenComponent ~ drop ~ event:", event)
     if (event.previousIndex === event.currentIndex) return;
 
     moveItemInArray(this.result, event.previousIndex, event.currentIndex);
-    console.log("🚀 ~ file: list-screen.component.ts:79 ~ ListScreenComponent ~ drop ~ this.result:", this.result)
-    // TODO: Reorder position also
-    console.log('list id: ', this.list.id!);
-    this.listService.update(this.list.id!, {...this.list, notes: this.result.map((x) => x.id!)})
-      .subscribe((x) => {
-        console.log(x);
-      });
-  }
-
-  orderPosition(index: number) {
-
-  }
-
-  onRenameList() {
-    console.log(this.list);
-    this.renameDialog.open(RenameDialogComponent, {
-      width: '250px',
-      data: this.list,
-    });
-  }
-
-  onOrderBy() {}
-
-  onDuplicateList() {
-    console.log('on duplicate list');
-    const newNotesArray: number[] = [];
-    const duplicatedList: List = {
-      dateCreated: Date.now().valueOf(),
-      position: this.list.notes.length + 1,
-      notes: [...this.list.notes],
-      title: `Copy of ${this.list.title}`,
-      isArchived: this.list.isArchived,
-      isFavorite: this.list.isFavorite,
+    for (const { index, value } of this.result.map((value, index) => ({ index, value }))) {
+      value.position = index;
+      firstValueFrom(this.noteService.update(value.id!, value));
     }
-    console.log("🚀 ~ file: list-screen.component.ts:128 ~ ListScreenComponent ~ onDuplicateList ~ duplicatedList:", duplicatedList)
-    // Clone notes
-    this.result.map((note: Note, index: number) => {
-      this.noteService.create(note.content, note.position, note.isFavorite)
-        .subscribe((createdNote: Note) => {
-          console.log("🚀 ~ file: list-screen.component.ts:143 ~ ListScreenComponent ~ .subscribe ~ createdNote:", createdNote)
-          newNotesArray.push(createdNote.id!);
 
-          if(index === this.result.length - 1) {
-            console.log("🚀 ~ file: list-screen.component.ts:146 ~ ListScreenComponent ~ .subscribe ~ newNotesArray:", newNotesArray)
-            // Create list
-            this.listService.create(`Copy of ${this.list.title}`,
-                                    this.list.notes.length + 1,
-                                    // [...this.list.notes],
-                                    [...newNotesArray],
-                                    this.list.isFavorite).subscribe((x) => {
-                                      console.log(x);
-                                    })
-          } else {
-            console.log(index);
+    // Save reorder position
+    this.subscriptions.push(
+      this.listService.update(this.list.id!, {...this.list, notes: this.result.map((x) => x.id!)})
+        .subscribe((res) => {
+          this.noteService.updateNotesSubject([...this.result]);
+          if (res.affected === 1) {
+            this.openSnackBar('Notes reordered', 'Close');
           }
         })
+    );
+  }
+
+  /**
+   * Opens the rename list dialog.
+   * @returns void.
+   */
+  onRenameList() {
+    this.subscriptions.push(
+      this.renameDialog.open(RenameDialogComponent, {
+        width: '250px',
+        data: this.list,
+      }).afterClosed().subscribe((newTitle: string) => {
+        if (newTitle) {
+          this.subscriptions.push(
+            this.listService.update(this.list.id!, {...this.list, title: newTitle})
+              .subscribe((res) => {
+                if (res.affected === 1) { 
+                  this.title = newTitle;
+                  this.openSnackBar('List renamed!', 'Close');
+              }
+            })
+          );
+        }
       })
+    );
   }
 
-  onDeleteList() {
-    const dialogResponse = this.confirmDialog.open(ConfirmDialogComponent, {
-      width: '250px',
-      data: {dialogContent: `Would you like to delete (${this.list.title}) note?`},
-    }).afterClosed().subscribe((x) => {
-      console.log(x);
-      if (x) {
-        // Delete list
-        this.listService.delete(this.list.id!).subscribe((y) => {
-          console.log(y);
-        })
-      }
-    });
+  /**
+   * Duplicates a list.
+   */
+  onDuplicateList(): void {
+    const newNotesArray: number[] = [];
+    // Clone notes
+    this.result.map((note: Note, index: number) => {
+      this.subscriptions.push(
+        this.noteService.create(note.content, note.position, note.isFavorite)
+          .subscribe((createdNote: Note) => {
+            newNotesArray.push(createdNote.id!);
+  
+            if(index === this.result.length - 1) {
+              // Create list
+              this.subscriptions.push(
+                this.listService.create(`Copy of ${this.list.title}`,
+                                        this.list.notes.length + 1,
+                                        [...newNotesArray],
+                                        this.list.isFavorite).subscribe((x) => {
+                                        this.openSnackBar('List duplicated!', 'Close');
+                                        })
+              );
+            } else {
+            }
+          })
+      );
+    })
   }
 
-  onSortListBy(option: number) {
-    console.log("🚀 ~ file: list-screen.component.ts:180 ~ ListScreenComponent ~ onSortListBy ~ option:", option)
+  /**
+   * Deletes a list.
+   * @returns void.
+   */
+  onDeleteList(): void {
+    this.subscriptions.push(
+      this.confirmDialog.open(ConfirmDialogComponent, {
+        width: '250px',
+        data: {dialogContent: `Would you like to delete (${this.list.title}) list?`},
+      }).afterClosed().subscribe((isDeleted) => {
+        if (isDeleted) {
+          // Delete list
+          this.subscriptions.push(
+            this.listService.delete(this.list.id!, this.list.notes)
+              .subscribe(() => {
+                this.openSnackBar('List deleted!', 'Close');
+              })
+          );
+        }
+      })
+    );
+  }
+
+  /**
+   * Sorts a list by the selected parameter.
+   * @param option Parameter to order the list.
+   * @returns void.
+   */
+  onSortListBy(option: number): void {
     enum SortingOptions {
       'alphabetically',
       'creationDate',
       'importance'
     };
 
-    if (option < 0 || option > Object.keys(SortingOptions).length) return console.log('is null');
+    if (option < 0 || option > Object.keys(SortingOptions).length) return;
 
     switch (option) {
       case SortingOptions.alphabetically:
@@ -193,20 +220,28 @@ export class ListScreenComponent {
           if (x.content > y.content) { return 1; }
           return 0;
         })];
-        console.log(this.result);
         break;
 
       case SortingOptions.creationDate:
-        console.log('entering case 1')
         this.result = [...this.result.sort((x, y) => x.dateCreated - y.dateCreated)];
-        console.log(this.result);
         break;
         
       case SortingOptions.importance:
         this.result = [...this.result.sort((x, y) => {return (x.isFavorite === y.isFavorite)? 0 : x.isFavorite? -1 : 1;})];
-        console.log(this.result);
         break;
     }
+  }
+
+  onEmitDailyStatus(index: number, value: boolean) {
+    this.changedDailyStatus.emit({index, value});
+  }
+
+  onEmitCompletedStatus(index: number, value: boolean) {
+    this.changedCompletedStatus.emit({index, value});
+  }
+
+  openSnackBar(message: string, action: string) {
+    this.snackbar.open(message, action);
   }
 
   ngOnDestroy(): void {
